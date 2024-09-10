1e07bbd2814f3bd160731ce321713bc6bb269e90
Hernan <hernanvallejos94@gmail.com>
2024-09-10 08:48:16 -0300

    feature: new form telescopica

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { FormularioSelectorComponent } from './modules/formulario-selector/formulario-selector.component';
 import { FormComponent } from './modules/form/form.component';
 import { FormMixerComponent } from './modules/form-mixer/form-mixer.component';
+import { FormTelescopicaComponent } from './modules/form-telescopica/form-telescopica.component';
 
 const routes: Routes = [
   { path: '', component: FormularioSelectorComponent }, // Ruta por defecto
   { path: 'puente', component: FormComponent },
   { path: 'mixer', component: FormMixerComponent },
+  { path: 'telescopica', component: FormTelescopicaComponent },
   { path: '**', redirectTo: '' } // Redireccionar cualquier ruta no existente a la selección de formulario
 ];
 @NgModule({
---
+++ src/app/modules/form-telescopica/form-telescopica.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-telescopica',
+  templateUrl: './form-telescopica.component.html',
+  styleUrl: './form-telescopica.component.scss'
+})
+export class FormTelescopicaComponent {
+
+}
